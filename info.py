7701738dae1dc8de9959c7a38913e7f4f989b6b6
Rexfilers <138610025+Rexfilers@users.noreply.github.com>
2023-08-15 20:39:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "6349079816:AAGJoqv2OnmBiZTmoUVNAZWBcbVj75DmTIs")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rmbbots:RaMbO999@cluster0.0831roz.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
